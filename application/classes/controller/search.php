2c2a1e46cc90ed50d5f4db090cf0b47c7d75f295
Kevin de Quillettes <k.dequillettes@gmail.com>
2012-05-03 13:40:29 +0200

    Added the start of the search controller

--- application/classes/controller/search.php
+++ application/classes/controller/search.php
@@ -2,9 +2,28 @@
 
 class Controller_Search extends Controller_Template_Website {
 
+	private static final $idfThreshold = 0.350;
+
 	public function action_search()
 	{
+		// Get the query string
+		$query = $this->request->param('q');
+		
+		// Build a vector from the query
+		$queryVector = array();
+		foreach(explode($query) as $word) {
+			$idf = DB::query(Database::SELECT, 'SELECT idf FROM Monumentzo.TermIndex');
+			
+			if($idf > $idfThreshold) {			
+				$queryVector[$word] = ($queryVector[$word]) ? $queryVector[$word] += 1 : 1;
+			}
+		}
+		
+		
+		
+		$results = array();
+		
 		$this->template->title = 'Search results';
-        $this->template->content = View::factory('search/results');
+        $this->template->content = View::factory('search/results')->set('results', results);
 	}
 }
