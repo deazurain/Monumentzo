61fcde91ec61519b2eff6f881a98871730590665
Kevin de Quillettes <k.dequillettes@gmail.com>
2012-05-04 18:05:39 +0200

    Finished the tagger, but I haven't tested it yet and haven't run it yet, so it may break your computer

---
+++ offline/Tagger/src/org/Monumentzo/Tagger/DatabaseWriter.java
@@ -0,0 +1,84 @@
+package org.Monumentzo.Tagger;
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+
+public class DatabaseWriter {
+
+	private Connection dbConnection = null;
+	private String currentDatabase = "";
+	
+	public DatabaseWriter(String databaseURL, String Database, String user, String password) throws SQLException {
+		currentDatabase = Database;		
+		
+		try {
+			// Create a connection to the database
+			Class.forName("com.mysql.jdbc.Driver");
+			dbConnection = (Connection) DriverManager.getConnection(databaseURL, user, password);
+	        
+			// Select the wanted database
+			Statement stmt = (Statement) dbConnection.createStatement();
+	        stmt.executeQuery("USE " + currentDatabase);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		
+		if(dbConnection == null) {
+			throw new SQLException("Could not connect to the specified database.");
+		}
+	}
+	
+	public void writeTf_IdfInformation(int monumentID, String word, double tf, double idf, double tfIdf) {
+		
+		try {
+			// Insert the tag
+			PreparedStatement insertTag =
+					dbConnection.prepareStatement("INSERT INTO Monumentzo.TextTag (TextTag, InverseDocumentFrequency) " +
+												  "VALUES (?, ?);");
+			insertTag.setString(1, word);
+			insertTag.setDouble(2, idf);
+			insertTag.execute();
+			
+			// Get the generated ID of the tag that was inserted above 
+			PreparedStatement tagIDRetrieving = dbConnection.prepareStatement("SELECT TextTagID" +
+																			  "FROM Monumentzo.TextTag" +
+																			  "WHERE TextTag = ?;");
+			tagIDRetrieving.setString(1, word);
+			ResultSet tag = tagIDRetrieving.executeQuery();
+			
+			int tagID = tag.getInt("TextTagID");
+			
+			// Insert the link of the monument to the tag
+			PreparedStatement insertMonument_TagLink = 
+				dbConnection.prepareStatement("INSERT INTO Monumentzo.Monument_TextTag (MonumentID, TextTagID, TermFrequencyInverseDocumentFrequency, TermFrequency)" +
+											  "VALUES (?, ?, ?, ?);");
+			insertMonument_TagLink.setInt(1, monumentID);
+			insertMonument_TagLink.setInt(2, tagID);
+			insertMonument_TagLink.setDouble(3, tfIdf);
+			insertMonument_TagLink.setDouble(4, idf);
+			insertMonument_TagLink.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void writeMonumentVector(int monumentID, String vector) {
+		
+		try {
+			// Write the vector representation of the monument to the database
+			PreparedStatement insertVector = 
+					dbConnection.prepareStatement("UPDATE Monumentzo.Monument SET Vector = ? WHERE MonumentID = ?;");
+			insertVector.setString(1, vector);
+			insertVector.setInt(2, monumentID);
+			insertVector.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
