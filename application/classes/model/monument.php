932a02264a13a7fee91e0d356f54a3dd0369f57a
Salim Salmi <salmi0707@gmail.com>
2012-05-04 10:41:02 +0200

    adjustment model

--- application/classes/model/monument.php
+++ application/classes/model/monument.php
@@ -1,42 +1,42 @@
 <?php
 
 /**
- * The User class allows to log a user in or out
- * and register them, next to modifying and getting
- * information of the user. 
+ * The Monument Model.
  */
 class Model_Monument extends Model_Database {
 
-	private $monumentID;
-	private $name;
-	private $discription;
-	private $latitude;
-	private $longitude;
-	private $city;
-	private $province;
-	private $street;
-	private $streetNumberText;
-	private $foundationDateText;
-	private $attributes;
-	private $categories;
+	$monumentID;
+	$name;
+	$discription;
+	$latitude;
+	$longitude;
+	$city;
+	$province;
+	$street;
+	$streetNumberText;
+	$foundationDateText;
+	$attributes;
+	$categories;
 
 	public function __construct($id){
 		parent::__construct()
 		$select = DB::query(Database::SELECT, 'SELECT * FROM Monument WHERE MonumentID = :id')
 	   	->bind(':id', $id)
 		$result = $select->execute();
+		
 		$monument = $result[0]
-		$name = $monument->Name;
-		$discription = $monument->Discription;
-		$latitude = $monument->Latitude;
-		$longitude = $monument->Latitude;
-		$city = $monument->City;
-		$province = $monument->Province;
-		$street = $monument->Street;
-		$streetNumberText = $monument->StreetNumberText;
-		$foundationDateText = $monument->FoundationDateText;
-		$attributes = $monument->Attributes;
-		$categories = $monument->Categories;
+		
+		$name 					= $monument->Name;
+		$discription 			= $monument->Discription;
+		$latitude 				= $monument->Latitude;
+		$longitude				= $monument->Latitude;
+		$city 					= $monument->City;
+		$province 				= $monument->Province;
+		$street		 			= $monument->Street;
+		$streetNumberText 	= $monument->StreetNumberText;
+		$foundationDateText 	= $monument->FoundationDateText;
+		$attributes 			= $monument->Attributes;
+		$categories 			= $monument->Categories;
 		}
 	}
 	
