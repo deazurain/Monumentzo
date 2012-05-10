49eb7fa85ae6c11b447113be55bfeaeb52855bc4
Kevin de Quillettes <k.dequillettes@gmail.com>
2012-05-10 10:57:16 +0200

    Added the view for the browse function & linked the three.js library

---
+++ application/views/browse/browse.php
@@ -0,0 +1,8 @@
+<div class="container-fluid">
+	<script type="text/javascript" src="assets/js/three.js" language="javascript"></script>
+    <script type="text/javascript" src="assets/js/browse.js" language="javascript"></script>
+	<div id="menu" class="span4"></div>
+	<div class="span12">
+    	<canvas></canvas>
+    </div>
+</div>
