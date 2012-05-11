6d57c84b8b791daef0d11e6af29da791e3179edb
Kevin de Quillettes <k.dequillettes@gmail.com>
2012-05-11 10:30:27 +0200

    The browser should now be able to load the monuments and dispaly a cube for each monument

--- assets/js/browse.js
+++ assets/js/browse.js
@@ -8,82 +8,76 @@ window.innerHeight = 800;
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 
-document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-
-init();
-animate();
-
-function init() {
-
-	container = $( '<div></div>' ).attr( 'id', 'browseWindow' );;
-	$(document.body).append( container );
-
-	scene = new THREE.Scene();
-	scene.fog = new THREE.Fog( 0xffffff, 1, 10000 );
-
-	camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 10000 );
-	camera.position.z = 500;
-	scene.add( camera );
-
-	var geometry = new THREE.CubeGeometry( 100, 100, 100 );
-	var material = new THREE.MeshNormalMaterial();
-
-	group = new THREE.Object3D();
-
-	for ( var i = 0; i < 1000; i ++ ) {
-
-		var mesh = new THREE.Mesh( geometry, material );
-		mesh.position.x = Math.random() * 2000 - 1000;
-		mesh.position.y = Math.random() * 2000 - 1000;
-		mesh.position.z = Math.random() * 2000 - 1000;
-
-		mesh.rotation.x = Math.random() * 360 * ( Math.PI / 180 );
-		mesh.rotation.y = Math.random() * 360 * ( Math.PI / 180 );
-
-		mesh.matrixAutoUpdate = false;
-		mesh.updateMatrix();
-
-		group.add( mesh );
-
-	}
-
-	scene.add( group );
-
-	renderer = new THREE.WebGLRenderer();
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	renderer.sortObjects = false;
-
-	container.append( renderer.domElement );
-}
-
-function onDocumentMouseMove(event) {
-
+$(document).on('mousemove', function (event) {
 	mouseX = ( event.clientX - windowHalfX ) * 10;
 	mouseY = ( event.clientY - windowHalfY ) * 10;
-}
+});
 
-function animate() {
-
-	requestAnimationFrame( animate );
-	render();
-}
-
-function render() {
-
-	var time = Date.now() * 0.001;
-
-	var rx = Math.sin( time * 0.7 ) * 0.5,
-		ry = Math.sin( time * 0.3 ) * 0.5,
-		rz = Math.sin( time * 0.2 ) * 0.5;
-
-	camera.position.x += ( mouseX - camera.position.x ) * .05;
-	camera.position.y += ( - mouseY - camera.position.y ) * .05;
-
-	camera.lookAt( scene.position );
-
-	group.rotation.x = rx;
-	group.rotation.y = ry;
-	group.rotation.z = rz;
-
-	renderer.render( scene, camera );
-}
+$.getJSON('/browse/info', function(data) {
+	
+	(function init() {
+	
+		container = $( '<div></div>' ).attr( 'id', 'browseWindow' );;
+		$(document.body).append( container );
+	
+		scene = new THREE.Scene();
+		scene.fog = new THREE.Fog( 0xffffff, 1, 10000 );
+	
+		camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 10000 );
+		camera.position.z = 500;
+		scene.add( camera );
+	
+		var geometry = new THREE.CubeGeometry( 100, 100, 100 );
+		var material = new THREE.MeshNormalMaterial();
+	
+		group = new THREE.Object3D();
+	
+		$.each(data, function(index, monument) {
+			var mesh = new THREE.mesh(geometry, material);
+			mesh.position.x = (index % 5) * 120;
+			mesh.position.y = (index % 5) * 120;
+			mesh.position.z = 0;
+			
+			mesh.rotation.x = 0;
+			mesh.rotation.y = 0;
+			mesh.matrixAutoUpdate = false;
+			mesh.updateMatrix();
+			
+			group.add(mesh);
+		});
+	
+		scene.add( group );
+	
+		renderer = new THREE.WebGLRenderer();
+		renderer.setSize( window.innerWidth, window.innerHeight );
+		renderer.sortObjects = false;
+	
+		container.append( renderer.domElement );
+	})();
+	
+	(function animate() {
+	
+		requestAnimationFrame( animate );
+		render();
+	})();
+	
+	function render() {
+	
+		var time = Date.now() * 0.001;
+	
+		var rx = Math.sin( time * 0.7 ) * 0.5,
+			ry = Math.sin( time * 0.3 ) * 0.5,
+			rz = Math.sin( time * 0.2 ) * 0.5;
+	
+		camera.position.x += ( mouseX - camera.position.x ) * .05;
+		camera.position.y += ( - mouseY - camera.position.y ) * .05;
+	
+		camera.lookAt( scene.position );
+	
+		group.rotation.x = rx;
+		group.rotation.y = ry;
+		group.rotation.z = rz;
+	
+		renderer.render( scene, camera );
+	}
+});
