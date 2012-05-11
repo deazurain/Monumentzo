1ffb8e5b6a5f669f12fe3fb197b39219b39bcade
Kevin de Quillettes <k.dequillettes@gmail.com>
2012-05-11 11:04:33 +0200

    I hope I fixed the login/logout/register function

--- application/classes/auth.php
+++
@@ -1,5 +0,0 @@
-<?php defined('SYSPATH') or die('No direct access allowed.');
-
-class Auth extends Monumentzo_Auth {}
-
-?>
--- application/classes/monumentzo/auth.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class Monumentzo_Auth extends Kohana_Auth {
-
-}
-
-?>
