d4f57675e0531d6934e6434b5ea4dcd566289fec
Kevin de Quillettes <k.dequillettes@gmail.com>
2012-05-21 12:19:23 +0200

    Added basic Google Maps functionality

---
+++ assets/js/favorite.js
@@ -0,0 +1,7 @@
+var myOptions = {
+	center: new google.maps.LatLng(-34.397, 150.644),
+	zoom: 8,
+	mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+
+var map = new google.maps.Map($('#map_canvas'), myOptions);
