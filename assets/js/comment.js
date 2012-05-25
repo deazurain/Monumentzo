dba153fc58049b946188f34f81a294808cd42b9d
Kevin de Quillettes <k.dequillettes@gmail.com>
2012-05-25 11:42:32 +0200

    Changed the user message when the user successfully places comments

--- assets/js/comment.js
+++ assets/js/comment.js
@@ -32,14 +32,13 @@ $(document).ready(function () {
 			success: function(data) {
 				if(data.status == "success") {
 					// successfully logged in
-					form.find('.success-container').html('yay!').show();
-                    console.log(data.result);
+					form.find('.success-container').html('Uw commentaar is succesvol geplaatst').show();
 				}
 				else if(data.status == "fail") {
 					// failed to log in
 	
 					var errors = data.result;
-					var errorhtml = '<h4 class="alert-heading">Please revise the following fields</h4>';
+					var errorhtml = '<h4 class="alert-heading"></h4>';
 
 					for(var i = 0, length = errors.length; i < length; i++) {
 						errorhtml += '<li>' + errors[i] + '</li>';
