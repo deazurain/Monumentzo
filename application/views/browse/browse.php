c85f030d9004cbe39467e434e4ad2f5ee6c28890
Kevin de Quillettes <k.dequillettes@gmail.com>
2012-06-01 09:48:07 +0200

    Changed the browse menu. Testing a new style.

--- application/views/browse/browse.php
+++ application/views/browse/browse.php
@@ -1,12 +1,16 @@
-<div id="browse-menu span4 offset2">
+<div id="browse-menu" class="span4">
 	<div class="browse-menu-header">
     	<h2>Bladeren</h2>
     </div>
-    <div class="browse-menu-body">
-    	<label class="checkbox"><input type="checkbox" />Plaats</label>
+    <div class="browse-menu-body btn-group" data-toggle="buttons-checkbox">
+    	<button class="btn">Plaats</button>
+        <button class="btn">Tijd</button>
+        <button class="btn">Categorie</button>
+        <button class="btn">Attribuut</button>
+    	<!--<label class="checkbox"><input type="checkbox" />Plaats</label>
         <label class="checkbox"><input type="checkbox" />Tijd</label>
         <label class="checkbox"><input type="checkbox" />Categorie</label>
-        <label class="checkbox"><input type="checkbox" />Attribuut</label>
+        <label class="checkbox"><input type="checkbox" />Attribuut</label>-->
     </div>
     <div class="browse-menu-footer"></div>
 </div>
