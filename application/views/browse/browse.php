57aae7357d0c76c96527a19fa447cd6a2ae57087
Kevin de Quillettes <k.dequillettes@gmail.com>
2012-06-01 10:17:24 +0200

    Finalized the css part of the browse menu.

--- application/views/browse/browse.php
+++ application/views/browse/browse.php
@@ -1,15 +1,13 @@
-<div id="browse-menu" class="span3">
-	<div class="browse-menu-header">
-    	<!--<h2>Bladeren</h2>-->
-    </div>
+<div id="browse-menu">
+	<div class="browse-menu-header"></div>
     <div class="browse-menu-body">
-    	<div class="btn-group" data-toggle="buttons-checkbox">
+    	<div class="btn-group pull-left" data-toggle="buttons-checkbox">
             <button class="btn">Plaats</button>
             <button class="btn">Tijd</button>
             <button class="btn">Categorie</button>
             <button class="btn">Attribuut</button>
-            <button class="btn btn-danger">Reset</button>
         </div>
+        <button class="btn btn-danger pull-right">Reset</button>
     </div>
     <div class="browse-menu-footer"></div>
 </div>
