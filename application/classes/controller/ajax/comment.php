b206a1df35349bcec2a711b6f0a9e7017fec49f1
Mick <mickfreeze@gmail.com>
2012-06-01 11:44:20 +0200

    jajajajajajajaja ogogogogogo clickable enzo!!!! lol

--- application/classes/controller/ajax/comment.php
+++ application/classes/controller/ajax/comment.php
@@ -1,60 +1,60 @@
 <?php defined('SYSPATH') OR die('No Direct Script Access');
 
 /**
-* Javascript called Comment Controller
-* 
-* The Comment Controller class allows a user to place a comment through an ajax request.
-* 
-* @author Monumentzo Team
-*/
+ * Javascript called Comment Controller
+ *
+ * The Comment Controller class allows a user to place a comment through an ajax request.
+ *
+ * @author Monumentzo Team
+ */
 
 Class Controller_Ajax_Comment extends Controller_Template_Ajax {
 
-    public function action_create() {
+	public function action_create() {
 
-        $errors = array();
+		$errors = array();
 
-        $user = Auth::instance()->get_user();
+		$user = Auth::instance()->get_user();
 
-        if(!$user) {
-            $errors[] = 'You need to be logged in to place a comment';
+		if (!$user) {
+			$errors[] = 'You need to be logged in to place a comment';
 
-            $this->json_fail($errors);
-        }
+			$this->json_fail($errors);
+		}
 
-        $data = $_POST;
-        $data['UserID'] = $user->UserID;
-        $data['PlaceDate'] = date( 'Y-m-d H:i:s', time());
+		$data = $_POST;
+		$data['UserID'] = $user->UserID;
+		$data['PlaceDate'] = date('Y-m-d H:i:s', time());
 
-        $c = Model::factory('comment');
-        $c->set($data);
+		$c = Model::factory('comment');
+		$c->set($data);
 
-        $v = $c->validator();
+		$v = $c->validator();
 
-        if(!$v->check()) {
+		if (!$v->check()) {
 
-            $errors = array_merge($errors, array_keys($v->errors()));
+			$errors = array_merge($errors, array_keys($v->errors()));
 
-            $this->json_fail($errors);
+			$this->json_fail($errors);
 
-        }
-        else {
+		}
+		else {
 
-            $c->save();
+			$c->save();
 
-            $result= $c->get(); // get comment data as array
+			$result = $c->get(); // get comment data as array
 
-						$commentView = View::factory('model/comment');
-						$commentView->set('id', $result['CommentID']);
-						$commentView->set('name', $user->Name);
-						$commentView->set('placeDate', $result['PlaceDate']);
-						$commentView->set('comment', $result['Comment']);
-						$commentView->set('owner', ($user && ($user->UserID === $result['UserID'])) ? true : false);
+			$commentView = View::factory('model/comment');
+			$commentView->set('id', $result['CommentID']);
+			$commentView->set('name', $user->Name);
+			$commentView->set('placeDate', $result['PlaceDate']);
+			$commentView->set('comment', $result['Comment']);
+			$commentView->set('owner', ($user && ($user->UserID === $result['UserID'])) ? true : false);
 
-            $this->json_success((string)$commentView);
+			$this->json_success((string)$commentView);
 
-        }
-    }
+		}
+	}
 
 	public function action_remove() {
 
@@ -62,7 +62,7 @@ Class Controller_Ajax_Comment extends Controller_Template_Ajax {
 
 		$user = Auth::instance()->get_user();
 
-		if(!$user) {
+		if (!$user) {
 			$errors[] = 'You need to be logged in to remove a comment';
 
 			$this->json_fail($errors);
@@ -76,7 +76,7 @@ Class Controller_Ajax_Comment extends Controller_Template_Ajax {
 			->where('CommentID', '=', $data['CommentID'])
 			->and_where('UserID', '=', $data['UserID'])->execute();
 
-		if($r->count() === 1) {
+		if ($r->count() === 1) {
 			DB::delete('Comment')->where('CommentID', '=', $data['CommentID'])->execute();
 			$this->json_success(null);
 		}
