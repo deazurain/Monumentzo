88dbdd2a4931379c3156527038cb1f6cdf76be48
Kevin de Quillettes <k.dequillettes@gmail.com>
2012-06-01 09:54:37 +0200

    Added a reset button.

--- application/views/browse/browse.php
+++ application/views/browse/browse.php
@@ -1,16 +1,17 @@
-<div id="browse-menu" class="span4">
+<div id="browse-menu" class="span3">
 	<div class="browse-menu-header">
     	<h2>Bladeren</h2>
     </div>
-    <div class="browse-menu-body btn-group" data-toggle="buttons-checkbox">
-    	<button class="btn">Plaats</button>
-        <button class="btn">Tijd</button>
-        <button class="btn">Categorie</button>
-        <button class="btn">Attribuut</button>
-    	<!--<label class="checkbox"><input type="checkbox" />Plaats</label>
-        <label class="checkbox"><input type="checkbox" />Tijd</label>
-        <label class="checkbox"><input type="checkbox" />Categorie</label>
-        <label class="checkbox"><input type="checkbox" />Attribuut</label>-->
+    <div class="browse-menu-body">
+    	<div class="btn-group" data-toggle="buttons-checkbox">
+            <button class="btn">Plaats</button>
+            <button class="btn">Tijd</button>
+            <button class="btn">Categorie</button>
+            <button class="btn">Attribuut</button>
+        </div>
+        <div class="btn-group">
+        	<button class="btn btn-danger">Reset</button>
+        </div>
     </div>
     <div class="browse-menu-footer"></div>
 </div>
