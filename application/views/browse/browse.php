3712ca1ff31e1d71e134796078fdd3f9b8b3a6eb
Kevin de Quillettes <k.dequillettes@gmail.com>
2012-06-01 09:18:39 +0200

    First layout of the browse menu.

--- application/views/browse/browse.php
+++ application/views/browse/browse.php
@@ -1,2 +1,15 @@
+<div id="browse-menu">
+	<div class="menu-header">
+    	<h2>Bladeren</h2>
+    </div>
+    <div class="menu-body">
+    	<label class="checkbox"><input type="checkbox" />Plaats</label>
+        <label class="checkbox"><input type="checkbox" />Tijd</label>
+        <label class="checkbox"><input type="checkbox" />Categorie</label>
+        <label class="checkbox"><input type="checkbox" />Attribuut</label>
+    </div>
+    <div class="menu-footer"></div>
+</div>
+
 <?php echo HTML::script('assets/js/three.js'), PHP_EOL ?>
 <?php echo HTML::script('assets/js/browse.js'), PHP_EOL ?>
