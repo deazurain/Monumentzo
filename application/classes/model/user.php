7acbaad56e377bf88ef5f3b25d350b124a432185
Kevin de Quillettes <k.dequillettes@gmail.com>
2012-06-07 13:30:08 +0200

    Changed a couple of queries

--- application/classes/model/user.php
+++ application/classes/model/user.php
@@ -7,21 +7,30 @@
 class Model_User extends Model_Monumentzo_User {
 	
 	public function getFavoritesList(){
-		$result = DB::query(Database::SELECT, 'SELECT MonumentID, Name FROM FavoriteList WHERE UserID = :id')->param(':id', $this->UserID)->execute();
+		$result = DB::query(Database::SELECT, 'SELECT MonumentID, Name 
+												FROM FavoriteList, Monument 
+												WHERE FavoriteList.MonumentID = Monument.MonumentID 
+												AND UserID = :id')->param(':id', $this->UserID)->execute();
 		$result = $result->as_array();
 		
 		return $result;
 	}
 	
 	public function getVisitedList(){
-		$result = DB::query(Database::SELECT, 'SELECT MonumentID, Name FROM VisitedList WHERE UserID = :id')->param(':id', $this->UserID)->execute();
+		$result = DB::query(Database::SELECT, 'SELECT MonumentID, Name 
+												FROM VisitedList, Monument 
+												WHERE VisitedList.MonumentID = Monument.MonumentID 
+												AND UserID = :id')->param(':id', $this->UserID)->execute();
 		$result = $result->as_array();
 		
 		return $result;
 	}
 	
 	public function getWishList(){
-		$result = DB::query(Database::SELECT, 'SELECT MonumentID, Name FROM WishList WHERE UserID = :id')->param(':id', $this->UserID)->execute();
+		$result = DB::query(Database::SELECT, 'SELECT MonumentID, Name 
+												FROM WishList, Monument 
+												WHERE WishList.MonumentID = Monument.MonumentID 
+												AND UserID = :id')->param(':id', $this->UserID)->execute();
 		$result = $result->as_array();
 		
 		return $result;
