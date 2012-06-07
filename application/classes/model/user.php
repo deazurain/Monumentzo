ed115120f140e11ee99c3a152d0a9a200cb1a4f9
Kevin de Quillettes <k.dequillettes@gmail.com>
2012-06-07 13:31:44 +0200

    Changed a couple of queries

--- application/classes/model/user.php
+++ application/classes/model/user.php
@@ -7,7 +7,7 @@
 class Model_User extends Model_Monumentzo_User {
 	
 	public function getFavoritesList(){
-		$result = DB::query(Database::SELECT, 'SELECT MonumentID, Name 
+		$result = DB::query(Database::SELECT, 'SELECT Monument.MonumentID, Name 
 												FROM FavoriteList, Monument 
 												WHERE FavoriteList.MonumentID = Monument.MonumentID 
 												AND UserID = :id')->param(':id', $this->UserID)->execute();
@@ -17,7 +17,7 @@ class Model_User extends Model_Monumentzo_User {
 	}
 	
 	public function getVisitedList(){
-		$result = DB::query(Database::SELECT, 'SELECT MonumentID, Name 
+		$result = DB::query(Database::SELECT, 'SELECT Monument.MonumentID, Name 
 												FROM VisitedList, Monument 
 												WHERE VisitedList.MonumentID = Monument.MonumentID 
 												AND UserID = :id')->param(':id', $this->UserID)->execute();
@@ -27,7 +27,7 @@ class Model_User extends Model_Monumentzo_User {
 	}
 	
 	public function getWishList(){
-		$result = DB::query(Database::SELECT, 'SELECT MonumentID, Name 
+		$result = DB::query(Database::SELECT, 'SELECT Monument.MonumentID, Name 
 												FROM WishList, Monument 
 												WHERE WishList.MonumentID = Monument.MonumentID 
 												AND UserID = :id')->param(':id', $this->UserID)->execute();
