d2e299a521b5207d5b8d35d23cfd905226bafe56
Kevin de Quillettes <k.dequillettes@gmail.com>
2012-06-08 09:26:17 +0200

    Adding more functionality to the lists view page.

--- application/views/lists/view.php
+++ application/views/lists/view.php
@@ -1,46 +1,62 @@
-<div class="span4">
-	
-    <!-- Favorite list -->
-    <div class="row">
-    	<div class="page-header"><h3>Favorieten</h3></div>
-    	<table class="table striped-table">
-            <tr>
-                <th>#</th>
-                <th>Name</th>
-            </tr>
-            <?php foreach($favorites as $favorite): ?>
-            	<tr>
-                	<td><?php echo $favorite['MonumentID']; ?></td>
-                    <td><?php echo $favorite['Name']; ?></td>
+<div id="list-view">
+    <div class="span4">
+        
+        <!-- Favorite list -->
+        <div class="row">
+            <div class="page-header"><h3>Favorieten</h3></div>
+            <table class="table striped-table">
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
                 </tr>
-            <?php endforeach; ?>
-        </table>
+                <?php foreach($favorites as $monument): ?>
+                    <tr>
+                        <td><?php echo $monument['MonumentID']; ?></td>
+                        <td><?php echo $monument['Name']; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
+        </div>
+        
+        <!-- Visited list -->
+        <div class="row">
+            <div class="page-header"><h3>Bezochte monumenten</h3></div>
+            <table class="table striped-table">
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                </tr>
+                <?php foreach($visited as $monument): ?>
+                    <tr>
+                        <td><?php echo $monument['MonumentID']; ?></td>
+                        <td><?php echo $monument['Name']; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
+        </div>
+        
+        <!-- Wish list -->
+        <div class="row">
+            <div class="page-header"><h3>Te bezoeken monumenten</h3></div>
+            <table class="table striped-table">
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                </tr>
+                <?php foreach($wish as $monument): ?>
+                    <tr>
+                        <td><?php echo $monument['MonumentID']; ?></td>
+                        <td><?php echo $monument['Name']; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
+        </div>
     </div>
     
-    <!-- Visited list -->
-    <div class="row">
-	    <div class="page-header"><h3>Bezochte monumenten</h3></div>
-    	<table class="table striped-table">
-            <tr>
-                <th>#</th>
-                <th>Name</th>
-            </tr>
-        </table>
+    <!-- Google Maps -->
+    <div class="span8">
+        <div id="map_canvas"></div>
     </div>
-    
-    <!-- Wish list -->
-    <div class="row">
-	    <div class="page-header"><h3>Te bezoeken monumenten</h3></div>
-    	<table class="table striped-table">
-            <tr>
-                <th>#</th>
-                <th>Name</th>
-            </tr>
-        </table>
-    </div>
-</div>
-<div class="span8">
-	<div id="map_canvas"></div>
 </div>
 <?php echo HTML::script('http://maps.googleapis.com/maps/api/js?key=AIzaSyArtULnydU1gg4DjNfCvhXZx5Sq49p1ktg&sensor=false'), PHP_EOL ?>
 <?php echo HTML::script('assets/js/favorite.js'), PHP_EOL ?>
