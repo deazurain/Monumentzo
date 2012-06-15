14dc195a5dccb7f644726f3dc1d7cd3083c0b5cf
Bojana Dumeljic <silvertwilightz@gmail.com>
2012-06-15 21:09:50 +0200

    Footer fix.

--- application/views/browse/browse.php
+++ application/views/browse/browse.php
@@ -1,6 +1,5 @@
 <div id="browse-window">
     <h3 class="hud-title"></h3>
-    <div class="hud-top"></div>
     <div class="hud-help"><button class="btn btn-info"><i class="icon-question-sign icon-white"></button></div>
     <div class="hud-right"><button class="btn btn-inverse"><i class="icon-arrow-right icon-white"></i></button></div>
     <div class="hud-left"><button class="btn btn-inverse"><i class="icon-arrow-left icon-white"></i></button></div>
