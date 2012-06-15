b64eb4e8a1c1d89c78a09d2f84406770489dfa2f
Bojana Dumeljic <silvertwilightz@gmail.com>
2012-06-15 21:28:16 +0200

    Show videos on monument page.

--- assets/js/ytplayer.js
+++ assets/js/ytplayer.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     var params = { allowScriptAccess: "always" };
     var atts = { id: "myytplayer" };
     swfobject.embedSWF("http://www.youtube.com/apiplayer?enablejsapi=1&version=3&playerapiid=ytplayer",
-                       "myytplayer", "425", "356", "8", null, null, params, atts);
+                       "myytplayer", "570", "398", "8", null, null, params, atts);
 });
 
 function onYouTubePlayerReady(playerId) {
