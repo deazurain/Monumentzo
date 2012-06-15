d77d8c37ca3eee1485f77e520d0008244d4188e6
Bojana Dumeljic <silvertwilightz@gmail.com>
2012-06-15 19:32:48 +0200

    Browse buttons.

--- assets/js/browse.js
+++ assets/js/browse.js
@@ -390,7 +390,7 @@ $(document).ready(function() {
         $("#browse-window .hud-left").click(doRotateLeft);
         $("#browse-window .hud-zoomin").click(doZoomIn);
         $("#browse-window .hud-zoomout").click(doZoomOut);
-        $("#browse-window .hud-help").click());
+        $("#browse-window .hud-help").click();
 
         (function animate() {
             requestAnimationFrame( animate );
