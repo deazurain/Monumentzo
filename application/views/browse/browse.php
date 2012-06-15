623d73102b931213ff29aa3289a752d70d31302c
Bojana Dumeljic <silvertwilightz@gmail.com>
2012-06-15 19:16:55 +0200

    Browse buttons.

--- application/views/browse/browse.php
+++ application/views/browse/browse.php
@@ -3,10 +3,10 @@
     <div class="hud-top"></div>
     <div class="hud-right"><button class="btn-inverse"><i class="icon-arrow-right icon-white"></i></button></div>
     <div class="hud-bottom"></div>
-    <div class="hud-left"><button class="btn-inverse"><i class="icon-arrow-left icon-white"></i></button></div>
-    <div class="hud-zoomin"><button class="btn-inverse"><i class="icon-plus icon-white"></i></button></div>
-    <div class="hud-zoomout"><button class="btn-inverse"><i class="icon-minus icon-white"></i></button></div>
-    <div class="hud-help"><a class="btn-info" href="#helpbrowse"><i class="icon-info-sign icon-white"></i></button></div>
+    <div class="hud-left"><button class="btn btn-inverse"><i class="icon-arrow-left icon-white"></i></button></div>
+    <div class="hud-zoomin"><button class="btn btn-inverse"><i class="icon-plus icon-white"></i></button></div>
+    <div class="hud-zoomout"><button class="btn btn-inverse"><i class="icon-minus icon-white"></i></button></div>
+    <div class="hud-help"><a class="btn btn-info" href="#helpbrowse"><i class="icon-info-sign icon-white"></i></button></div>
 
     <div class="alert alert-info" style="display:none" id="browse-help">
         <a class="close" data-dismiss="alert" href="#"><i class="icon-remove"></i></a>
