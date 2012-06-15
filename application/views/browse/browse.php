112985a7131ea14a1cf32a2a84d7cdddd4977116
Bojana Dumeljic <silvertwilightz@gmail.com>
2012-06-15 17:13:46 +0200

    Browse button update.

--- application/views/browse/browse.php
+++ application/views/browse/browse.php
@@ -4,5 +4,5 @@
 	<div class="hud-top"></div>
 	<div class="hud-right"><button class="btn-inverse"><i class="icon-arrow-right icon-white"></i></button></div>
 	<div class="hud-bottom"></div>
-	<div class="hud-left"><button>&lt;</button></div>
+	<div class="hud-left"><button class="btn-inverse"><i class="icon-arrow-left icon-white"></i></button></div>
 </div>
