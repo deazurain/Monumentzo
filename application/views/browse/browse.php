ff23ccf277e0315358bc4eada4e566e4a4170136
Bojana Dumeljic <silvertwilightz@gmail.com>
2012-06-15 18:01:05 +0200

    Browse buttons.

--- application/views/browse/browse.php
+++ application/views/browse/browse.php
@@ -1,8 +1,8 @@
 
 <div id="browse-window">
 	<h3 class="hud-title"></h3>
-	<div class="hud-top"></div>
+	<div class="hud-top"><button class="btn-inverse"><i class="icon-arrow-up icon-white"></i></button></div>
 	<div class="hud-right"><button class="btn-inverse"><i class="icon-arrow-right icon-white"></i></button></div>
-	<div class="hud-bottom"></div>
+	<div class="hud-bottom"><button class="btn-inverse"><i class="icon-arrow-down icon-white"></i></button></div>
 	<div class="hud-left"><button class="btn-inverse"><i class="icon-arrow-left icon-white"></i></button></div>
 </div>
--- application/views/home.php
+++ application/views/home.php
@@ -4,24 +4,28 @@
     <h1>Welkom bij Monumentzo</h1>
     <div class="well black">
         <p>
-            Bekijk de mooiste monumenten en ontdek de blijde wereld van het culturele erfgoed van Nederland.</br>
-            Gebruik de blader functie om op een interactieve wijze de monumenten van de lage landen te verkennen.</br>
+            Bekijk de mooiste monumenten en ontdek de blijde wereld van het culturele erfgoed van Nederland.
+        </p>
+        <p>
+            Gebruik de blader functie om op een interactieve wijze de monumenten van de lage landen te verkennen.
+        </p> 
+        <p>
             Registreer, om een bericht bij je favoriete monumenten achter te laten en nog veel meer!
         </p>
     </div>
-    <p><a class="btn btn-primary btn-large" href="/browse/index">Bladeren »</a></p>
+    <p><a class="btn btn-primary btn-large" href="/browse/index">Bladeren <i class="icon-chevron-right icon-white"></i></a></p>
 </div>
 
 <!-- Example row of columns -->
 <div class="row preview-monument">
     <?php foreach($monuments as $monument) {?>
-    <div class="span4 overflow well">
+    <div class="span4 overflow">
         <h2><?= $monument['Name']; ?></h2>
         <p>
             <img src="/assets/img/monuments/thumb/<?= $monument['MonumentID']?>.jpg" align="left" />
             <?= $monument['Description']?>
         </p>
-        <p><a class="btn" href="/monument/view/<?= $monument['MonumentID'] ?>">Bekijk<i class="icon-chevron-right"></i></a></p>
+        <p><a class="btn" href="/monument/view/<?= $monument['MonumentID'] ?>">Bekijk <i class="icon-chevron-right"></i></a></p>
     </div>
     <?php } ?>
 </div>
