032e3ec1648f7b53af7e665395c1569ecd1be470
Mick <mickfreeze@gmail.com>
2012-06-15 21:08:45 +0200

    asdfasdfasdf

--- application/views/browse/browse.php
+++ application/views/browse/browse.php
@@ -1,7 +1,7 @@
 <div id="browse-window">
     <h3 class="hud-title"></h3>
     <div class="hud-top"></div>
-		<div class="hud-help"><button class="btn btn-primary pull-right"><i class="icon-question-sign icon-white"></button></div>
+		<div class="hud-help"><button class="btn btn-primary pull-right"><i class="icon-question-sign icon-white"></i></button></div>
 		<div class="hud-right"><button class="btn btn-inverse"><i class="icon-arrow-right icon-white"></i></button></div>
     <div class="hud-left"><button class="btn btn-inverse"><i class="icon-arrow-left icon-white"></i></button></div>
     <div class="hud-zoomin"><button class="btn btn-inverse"><i class="icon-plus icon-white"></i></button></div>
